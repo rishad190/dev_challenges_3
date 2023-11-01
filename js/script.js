433ad2b1fe50874ceb2ee234ace07f58e3737e4c
rishad khan <rishadkhan190@gmail..com>
2023-11-01 19:07:22 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // Add some interactivity to the website
-console.log("Hello, world!");
+function closeSideBox() {
+  document.getElementById("sideNav").style.display = "none";
+}
+function openSlideBox() {
+  document.getElementById("sideNav").style.display = "block";
+}
+
+function changeDark() {
+  document.body.classList.toggle("dark-mode");
+}
